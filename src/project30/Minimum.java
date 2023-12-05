9dc01834cccf3f9e535b272e9f4d3ce9a152104a
Sara Usevska <sarausevska42@yahoo.com>
2023-12-05 13:49:39 +0100

    first commit

---
+++ src/project30/Minimum.java
@@ -0,0 +1,15 @@
+package project30;
+
+public class Minimum {
+		public static int findMin(int n1, int n2) {
+			int min;
+			if(n1 < n2);
+			min=n1;
+		    min=2;
+		    
+			return min;
+		}
+		public static void main(String[]args) {
+		System.out.println("Minimum od dvata broja e:"+findMin(10,5));
+		}
+	}
